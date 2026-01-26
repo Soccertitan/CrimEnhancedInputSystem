cf95140f8710c323e3000339b2b32f0e72e2c2ae
Soccertitan <tristan.nowacki@outlook.com>
2026-01-26 08:56:10 -0800

    Created an Action for Debugging

---
+++ Source/CrimActionSystem/Private/Action/ActionDebug.cpp
@@ -0,0 +1,57 @@
+﻿// Copyright Soccertitan 2025
+
+
+#include "Action/ActionDebug.h"
+
+#include "InputActionValue.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UActionDebug::OnInputActionStarted(const FInputActionValue& Value)
+{
+	Super::OnInputActionStarted(Value);
+	if (bPrintActionStarted)
+	{
+		const FString Message = FString::Printf(TEXT("[%s] - Action Started: Value [%s]"), *GetName(), *Value.ToString());
+		UKismetSystemLibrary::PrintString(this, Message, bPrintToScreen, bPrintToLog, TextColor, Duration);
+	}
+}
+
+void UActionDebug::OnInputActionTriggered(const FInputActionValue& Value)
+{
+	Super::OnInputActionTriggered(Value);
+	if (bPrintActionTriggered)
+	{
+		const FString Message = FString::Printf(TEXT("[%s] - Action Triggered: Value [%s]"), *GetName(), *Value.ToString());
+		UKismetSystemLibrary::PrintString(this, Message, bPrintToScreen, bPrintToLog, TextColor, Duration);
+	}
+}
+
+void UActionDebug::OnInputActionOngoing(const FInputActionValue& Value)
+{
+	Super::OnInputActionOngoing(Value);
+	if (bPrintActionOngoing)
+	{
+		const FString Message = FString::Printf(TEXT("[%s] - Action Ongoing: Value [%s]"), *GetName(), *Value.ToString());
+		UKismetSystemLibrary::PrintString(this, Message, bPrintToScreen, bPrintToLog, TextColor, 0.f);
+	}
+}
+
+void UActionDebug::OnInputActionCanceled(const FInputActionValue& Value)
+{
+	Super::OnInputActionCanceled(Value);
+	if (bPrintActionCanceled)
+	{
+		const FString Message = FString::Printf(TEXT("[%s] - Action Canceled: Value [%s]"), *GetName(), *Value.ToString());
+		UKismetSystemLibrary::PrintString(this, Message, bPrintToScreen, bPrintToLog, TextColor, Duration);
+	}
+}
+
+void UActionDebug::OnInputActionCompleted(const FInputActionValue& Value)
+{
+	Super::OnInputActionCompleted(Value);
+	if (bPrintActionCompleted)
+	{
+		const FString Message = FString::Printf(TEXT("[%s] - Action Completed: Value [%s]"), *GetName(), *Value.ToString());
+		UKismetSystemLibrary::PrintString(this, Message, bPrintToScreen, bPrintToLog, TextColor, Duration);
+	}
+}
---
+++ Source/CrimActionSystem/Public/Action/ActionDebug.h
@@ -0,0 +1,43 @@
+﻿// Copyright Soccertitan 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CrimAction.h"
+#include "ActionDebug.generated.h"
+
+/**
+ * An action that prints debug strings for the different Input Action triggers.
+ */
+UCLASS()
+class CRIMACTIONSYSTEM_API UActionDebug : public UCrimAction
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere)
+	bool bPrintToScreen = true;
+	UPROPERTY(EditAnywhere)
+	bool bPrintToLog = true;
+	UPROPERTY(EditAnywhere)
+	FLinearColor TextColor = FLinearColor(0.0f, 0.66f, 1.0f);
+	UPROPERTY(EditAnywhere)
+	float Duration = 2.f;
+	
+	UPROPERTY(EditAnywhere)
+	bool bPrintActionStarted = true;
+	UPROPERTY(EditAnywhere)
+	bool bPrintActionTriggered = true;
+	UPROPERTY(EditAnywhere)
+	bool bPrintActionOngoing = true;
+	UPROPERTY(EditAnywhere)
+	bool bPrintActionCanceled = true;
+	UPROPERTY(EditAnywhere)
+	bool bPrintActionCompleted = true;
+	
+	virtual void OnInputActionStarted(const FInputActionValue& Value) override;
+	virtual void OnInputActionTriggered(const FInputActionValue& Value) override;
+	virtual void OnInputActionOngoing(const FInputActionValue& Value) override;
+	virtual void OnInputActionCanceled(const FInputActionValue& Value) override;
+	virtual void OnInputActionCompleted(const FInputActionValue& Value) override;
+};
